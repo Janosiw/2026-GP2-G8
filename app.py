39ad15512f59ebef9ceb31489dc500d380af1008
joudnaif <joudnaif99@gmail.com>
2026-05-16 23:32:46 +0300

    Fix Hugging Face port

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ import routes.reports
 import routes.profile
 
 if __name__ == "__main__":
-    shared.app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
